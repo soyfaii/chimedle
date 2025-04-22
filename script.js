45edb1299f235140037f4928339e84d1679971e9
Lucas Rubio <65956956+soyfaii@users.noreply.github.com>
2025-04-22 12:54:27 +0200

    Temporarily remove "latest game" button

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ fetch("resources/games.json")
   .then((response) => response.json())
   .then((data) => {
     // Latest game button
-    const latestGame = data[data.length - 1];
-    const latestGameLink = document.getElementById("button-latest");
-    latestGameLink.href = `game?id=${latestGame.id}`;
+    //const latestGame = data[data.length - 1];
+    //const latestGameLink = document.getElementById("button-latest");
+    //latestGameLink.href = `game?id=${latestGame.id}`;
     // Random game button
     const randomGameLink = document.getElementById("button-random");
     const randomIndex = Math.floor(Math.random() * data.length);
