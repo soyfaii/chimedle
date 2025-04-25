8383153ac209313f1d0ee9eb8de1164cb25cdacc
Lucas Rubio <65956956+soyfaii@users.noreply.github.com>
2025-04-25 16:48:18 +0200

    Color selection screen buttons according to game state

--- select/script.js
+++ select/script.js
@@ -1,3 +1,5 @@
+import LocalStorageManager from "../js/managers/LocalStorageManager.js";
+
 const gamesList = document.querySelector("ol");
 
 fetch("../resources/games.json")
@@ -7,8 +9,46 @@ fetch("../resources/games.json")
       const listItem = document.createElement("li");
       const link = document.createElement("a");
       link.href = `../game?id=${game.id}`;
-      link.textContent = `#${game.id}`;
-      link.className = "link-button";
+      link.textContent = `${game.id}`;
+      link.className = "link-button level-button";
+      // Make the color background match the game state
+      const saveFile = LocalStorageManager.loadGameState(game.id);
+      const gameState = (() => {
+        if (saveFile) {
+          const try1 = saveFile.try1;
+          const try2 = saveFile.try2;
+          const try3 = saveFile.try3;
+          if (
+            try1 == "guessedAll" ||
+            try2 == "guessedAll" ||
+            try3 == "guessedAll"
+          ) {
+            return "guessedAll";
+          } else if (
+            try1 == "guessedGame" ||
+            try2 == "guessedGame" ||
+            try3 == "guessedGame"
+          ) {
+            return "guessedGame";
+          } else if (try3 == "failed") {
+            return "failed";
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+      })();
+      if (gameState == "guessedAll" || gameState == "guessedGame") {
+        link.className += " green-bg";
+      } else if (gameState == "failed") {
+        link.className += " red-bg";
+      } else {
+        link.className += "";
+      }
+      if (gameState == "guessedAll") {
+        link.className += " button-star";
+      }
       listItem.appendChild(link);
       gamesList.appendChild(listItem);
     });
