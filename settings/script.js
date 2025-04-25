f8bf1c3c57491b7e90b8114f0c22a58c7ab275cc
Lucas Rubio <65956956+soyfaii@users.noreply.github.com>
2025-04-25 13:48:52 +0200

    make alert lowercase

--- settings/script.js
+++ settings/script.js
@@ -3,8 +3,8 @@ import LocalStorageManager from "../js/managers/LocalStorageManager.js";
 document
   .getElementById("button-delete-data")
   .addEventListener("click", function () {
-    if (confirm("Are you sure you want to delete all game data?")) {
+    if (confirm("are you sure you want to delete all game data?")) {
       LocalStorageManager.clearAllGameStates();
-      alert("All game data has been deleted.");
+      alert("all game data has been deleted.");
     }
   });
